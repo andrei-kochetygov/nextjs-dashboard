198e2a25c2f2657a38708e0ed9e0449cbac680dd
Andrei Kochetygov <kochetygov.andrey.ge@gmail.com>
2024-03-15 15:43:26 +0400

    initial commit

---
+++ .history/app/dashboard/customers/page_20240315145011.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+  return (
+    <main>Invoices Page</main>
+  );
+}
+  
---
+++ .history/app/dashboard/invoices/page_20240315144937.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return (
+      <main>Dashboard Page</main>
+    );
+  }
+  
